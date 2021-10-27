495670f1c0ba35775cbf5b136d9d40e1e436af1d
jagadish777 <52528218+jagadish777@users.noreply.github.com>
2021-10-27 19:34:11 +0530

    Update stc1.py

--- stc1.py
+++ stc1.py
@@ -23,19 +23,18 @@ df["Tumor_Prediction"] = df["Tumor_Prediction"].map({'Predicted':1,'NotPredicted
 data = df[["Head_Symptom1","Head_Symptom2","Head_Symptom3","Head_Symptom4","Head_Symptom5","Other_Symptom1","Other_Symptom2","Other_Symptom3","Other_Symptom4","Other_Symptom5","Tumor_Prediction"]].to_numpy()
 inputs = data[:,:-1]
 outputs = data[:, -1]
-training_inputs = inputs[:400]
-training_outputs = outputs[:400]
-testing_inputs = inputs[400:]
-testing_outputs = outputs[400:]
-estimators = [('rf', RandomForestClassifier(n_estimators=10, random_state=42)),('svr', make_pipeline(StandardScaler(),LinearSVC(random_state=42)))]
+training_inputs = inputs[:800]
+training_outputs = outputs[:800]
+testing_inputs = inputs[800:]
+testing_outputs = outputs[800:]
+estimators = [('rf', RandomForestClassifier(n_estimators=10)),('svr', make_pipeline(StandardScaler(),LinearSVC()))]
 classifier = StackingClassifier(estimators=estimators, final_estimator=LogisticRegression())
 #classifier = SVC()
 classifier.fit(training_inputs, training_outputs)
 predictions = classifier.predict(testing_inputs)
 accuracy = 100.0 * accuracy_score(testing_outputs, predictions)
-print(accuracy)
-"""""
 print ("The accuracy of Stacking Classifier on testing data is: " + str(accuracy))
+"""""
 testSet = [[1,0,0,0,0,1,0,0,0,1]]
 test = pd.DataFrame(testSet)
 predictions = classifier.predict(test)
